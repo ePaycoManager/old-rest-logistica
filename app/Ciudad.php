581d8894a1ea9fbca77dba0deb6bbccd70d287f9
juanMarulanda <david.marulanda@payco.co>
2018-09-19 10:24:12 -0500

    modificacion campo 'recogido' en operacion epayco a 'id_recogida'

--- app/Ciudad.php
+++ app/Ciudad.php
@@ -8,7 +8,7 @@ class Ciudad extends Model
 	protected $table = 'ciudades';
 	protected $fillable = ['codigo_dane', 'nombre', 'departamento','codigo_tcc','id_rest_payco'];
 	
-	protected $hidden = ['codigo_tcc','id_rest_payco'];
+	protected $hidden = ['id_rest_payco'];
 }
 ?>
 
